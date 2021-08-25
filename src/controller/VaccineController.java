566ddf552a56531304bff9622af17352f7d7ba8c
yyh <younghun7513@gmail.com>
2021-08-25 11:07:25 +0900

    merge2

--- src/controller/VaccineController.java
+++ src/controller/VaccineController.java
@@ -44,7 +44,6 @@ public class VaccineController {
 	public static void getVaccineByName(String vaccinName) {
 		if(vaccinName != null && !vaccinName.equals("")) {
 			Vaccine vaccine = VaccineDAO.getVaccineByName(vaccinName);
-			
 			if(vaccine != null) {
 				EndView.getVaccine(vaccine);
 			}else {
@@ -71,69 +70,50 @@ public class VaccineController {
 	}
 
 	
-	public static void addVaccine(Vaccine vaccine) {
+	//객체를 받아오는 방식으로 수정  // 데이터를 받아오는 곳, startView로 이동 예정.
+	public static void addVaccine(Vaccine vaccine ) {
+		
+//			Scanner sc = new Scanner(System.in);
+//			System.out.println("백신 명을 입력하세요");
+//			String vaccineName = sc.nextLine();
+//			System.out.println("백신의 접종 연령을 입력하세요");
+//			int targetAge = sc.nextInt();
+//			System.out.println("백신의 접종간격을 입력하세요");
+//			int period = sc.nextInt();
+//			System.out.println("플랫폼을 입력하세요");
+//			String platform = sc.nextLine();
+//			System.out.println("백신의 보관 온도를 입력하세요");
+//			String temperature = sc.nextLine();
+//			System.out.println("보관 기간을 숫자로 입력하세요");
+//			int storage = sc.nextInt();
+//			Vaccine vaccine = new Vaccine(vaccineName, targetAge, period, platform, temperature, storage);
+//		Vaccine vaccine = new Vaccine("얀센",50,0,"바이러스 벡터","-25도 ~ 15도",8);
+		
 		if(vaccine != null) {
-			Vaccine newVaccine = VaccineDAO.addVaccine(vaccine);
-			
-			if(newVaccine != null) {
-				EndView.addVaccineView(newVaccine);
-			}else {
-				EndView.errorMessage("새로운 백신 정보 추가를 실패했습니다.");
+			try {
+				if(VaccineDAO.addVaccine(vaccine)) {
+					EndView.showResult("백신 등록 성공");
+				}else {
+					EndView.errorMessage("백신 등록 실패");
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
 			}
-		}else {
-			EndView.errorMessage("아무것도 입력하지 않았거나 null값 입니다.");
 		}
 	}
 	
-	
-	
-//	public static void addVaccine(Vaccine vaccine) {
-//		// 데이터를 받아오는 곳.
-////			Scanner sc = new Scanner(System.in);
-////			System.out.println("백신 명을 입력하세요");
-////			String vaccineName = sc.nextLine();
-////			System.out.println("백신의 접종 연령을 입력하세요");
-////			int targetAge = sc.nextInt();
-////			System.out.println("백신의 접종간격을 입력하세요");
-////			int period = sc.nextInt();
-////			System.out.println("플랫폼을 입력하세요");
-////			String platform = sc.nextLine();
-////			System.out.println("백신의 보관 온도를 입력하세요");
-////			String temperature = sc.nextLine();
-////			System.out.println("보관 기간을 숫자로 입력하세요");
-////			int storage = sc.nextInt();
-////			Vaccine vaccine = new Vaccine(vaccineName, targetAge, period, platform, temperature, storage);
-//		
-////		Vaccine vaccine = new Vaccine("얀센",50,0,"바이러스 벡터","-25도 ~ 15도",8);
-//		
-//		if(vaccine != null) {
-//			Vaccine newVaccine = null;
-//			
-//			try {
-//				newVaccine = VaccineDAO.addVaccine(vaccine);
-//				if(newVaccine != null) {
-//					EndView.addVaccineView(newVaccine);
-//				}else {
-//					EndView.errorMessage("새로운 백신 정보 추가를 실패했습니다.");
-//				}
-//			}catch(Exception e) {
-//				EndView.errorMessage("<< 백신 정보 추가 실패 >>");
-//				e.printStackTrace();
-//			}
-//			
-//		}else {
-//			EndView.errorMessage("아무것도 입력하지 않았거나 null값 입니다.");
-//		}
-//	}
-	
 // -------------------------------------------------------------------------------
 	
-	// 이거는 제가 잘 모르겠어서 뒀습니다ㅜ
-	public static void updateVaccine(String vaccineName) {
+	// 이거는 제가 잘 모르겠어서 뒀습니다ㅜ    //파라미터로 받기  백신네임 플랫폼 온도
+	public static void updateVaccine(String vaccineName,String platform,String temper) {
 		if(vaccineName != null && !vaccineName.equals("")) {
 			//수정 데이터, 객체 입력 
-			Vaccine updateVaccine = new Vaccine(vaccineName,30,0,"바이러스 벡터","-25도 ~ 15도",8);
-			VaccineDAO.updateVaccine(updateVaccine);
+//			Vaccine updateVaccine = new Vaccine(vaccineName,30,0,"바이러스 벡터","-25도 ~ 15도",8);
+			if(VaccineDAO.updateVaccine(vaccineName,platform,temper)) {
+				EndView.showResult(vaccineName +"수정이 성공했습니다.");
+			}else {
+				EndView.errorMessage("접종 연령 수정을 실패했습니다.");
+			}
 		}else {
 			EndView.errorMessage("아무것도 입력하지 않았거나 null값 입니다.");
 		}
@@ -147,7 +127,7 @@ public class VaccineController {
 		if(vaccineName != null && !vaccineName.equals("") && (Integer)age !=null) {
 			
 			if(VaccineDAO.updateVaccineTargetAge(vaccineName, age)) {
-				EndView.showResult(vaccineName + "의 접종 연령을 " + age + "세로 수정 완료하였습니다.");
+				EndView.showResult(vaccineName + "의 접종 연령을 " + age + "새로 수정 완료하였습니다.");
 			}else {
 				EndView.errorMessage("접종 연령 수정을 실패했습니다.");
 			}
@@ -163,7 +143,11 @@ public class VaccineController {
 	public static void deleteVaccine(String vaccineName) {
 		if(vaccineName != null && !vaccineName.equals("")) {
 			
-			VaccineDAO.deleteVaccine(vaccineName);
+			if(VaccineDAO.deleteVaccine(vaccineName)) {
+				EndView.showResult("백신 삭제가 완료되었습니다.");
+			}else {
+				EndView.errorMessage("백신 삭제가 실패되었습니다.");
+			}
 		}else {
 			EndView.errorMessage("아무것도 입력하지 않았거나 null값 입니다.");
 		}
--- src/model/dao/VaccineDAO.java
+++ src/model/dao/VaccineDAO.java
@@ -9,34 +9,30 @@ import model.entity.Vaccine;
 import util.PublicCommon;
 import view.EndView;
 
-public class VaccineDAO {	
-	
+public class VaccineDAO {
+
 	/**
-	 * VaccineDAO 
-	 * - getAllVaccine
-	 * - ??중복?? getVaccine
-	 * - findByVaccineName -> getVaccineByName
-	 * - findByVaccineTagetAge -> getVaccineByTargetAge
+	 * VaccineDAO - getAllVaccine - ??중복?? getVaccine - findByVaccineName ->
+	 * getVaccineByName - findByVaccineTagetAge -> getVaccineByTargetAge
 	 * 
-	 * - addVaccine 
+	 * - addVaccine
 	 * 
-	 * - updateVaccine 
-	 * - updateVaccineAge -> updateVaccineTargetAge
+	 * - updateVaccine - updateVaccineAge -> updateVaccineTargetAge
 	 * 
 	 * - deleteVaccine
 	 */
-	
+
 	public static List<Vaccine> getAllVaccine() {
 
 		EntityManager em = PublicCommon.getEntityManager();
 		String query = "select v from Vaccine v";
 		List<Vaccine> vaccines = em.createQuery(query).getResultList();
 
-		if (vaccines.size() >0 ) {
+		if (vaccines.size() > 0) {
 			em.close();
 			em = null;
 			return vaccines;
-			
+
 		} else {
 			// 예외 발생 백신이 없는 상황
 			System.out.println("백신이 존재하지 않습니다.");
@@ -45,194 +41,145 @@ public class VaccineDAO {
 			return null;
 		}
 	}
-	
-	
-//	public static Vaccine getVaccine(String vaccinName) {
-//		if (vaccinName != null && !vaccinName.equals("")) {
-//			EntityManager em = PublicCommon.getEntityManager();
-//			EntityTransaction tx = em.getTransaction();
-//			Vaccine vaccine = null;
-//			tx.begin();
-//
-//			try {
-//				vaccine = em.find(Vaccine.class, vaccinName);
-//
-//				if (vaccine == null) {
-//					System.out.println("백신이 존재하지 않습니다.");
-//				}
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//				tx.rollback();
-//			} finally {
-//				em.close();
-//				em = null;
-//			}
-//
-//			return vaccine;
-//		} else {
-//			EndView.errorMessage("아무것도 입력하지 않았거나 null값 입니다.");
-//			return null;
-//		}
-//	}
 
-	
 	// 입력값 null 여부는 controller에서 체크하고 들어오지 않나용!?
 	public static Vaccine getVaccineByName(String vaccinName) {
-		if (vaccinName != null && !vaccinName.equals("")) {
-			EntityManager em = PublicCommon.getEntityManager();
-			try {
-				Vaccine vaccine = (Vaccine) em.createNamedQuery("Vaccine.findByVaccineName")
-						.setParameter("name", vaccinName).getSingleResult();
-				em.close();
-				em = null;
-				return vaccine;
-			} catch (Exception e) {
-				System.out.println("백신 정보가 존재하지 않습니다.");
-				e.printStackTrace();
-			}
-		} else {
-			EndView.errorMessage("아무것도 입력하지 않았거나 null값 입니다.");
-			return null;
+		EntityManager em = PublicCommon.getEntityManager();
+		try {
+			Vaccine vaccine = (Vaccine) em.createNamedQuery("Vaccine.findByVaccineName")
+					.setParameter("name", vaccinName).getSingleResult();
+			em.close();
+			em = null;
+			return vaccine;
+		} catch (Exception e) {
+			System.out.println("백신 정보가 존재하지 않습니다.");
+			e.printStackTrace();
 		}
 		return null;
 	}
 
-	//바로 위 메소드와 동일한 null 처리 방식!
+	// 바로 위 메소드와 동일한 null 처리 방식!
 	public static List<Vaccine> getVaccineByTargetAge(int targetAge) {
-		if ((Integer) targetAge != null) {
-			EntityManager em = PublicCommon.getEntityManager();
-			try {
-				List<Vaccine> vaccines = em.createNamedQuery("Vaccine.findByVaccineTargetAge")
-						.setParameter("target_age", targetAge).getResultList();
-				em.close();
-				em = null;
-				return vaccines;
-			} catch (Exception e) {
-				System.out.println("해당 연령의 백신 정보가 존재하지 않습니다.");
-				e.printStackTrace();
-			}
+		EntityManager em = PublicCommon.getEntityManager();
+		try {
+			List<Vaccine> vaccines = em.createNamedQuery("Vaccine.findByVaccineTargetAge")
+					.setParameter("target_age", targetAge).getResultList();
+			em.close();
+			em = null;
+			return vaccines;
+		} catch (Exception e) {
+			System.out.println("해당 연령의 백신 정보가 존재하지 않습니다.");
+			e.printStackTrace();
+			return null;
 		}
-		return null;
 	}
-	
+
 	// 추가 시 boolean 반환? 혹은 해당 객체 반환? 통일 필요!
-	public static Vaccine addVaccine(Vaccine vaccine) {
+	public static boolean addVaccine(Vaccine vaccine) {
+		EntityManager em = PublicCommon.getEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		boolean result = false;
+		try {
+			tx.begin();
+			em.persist(vaccine);
+			tx.commit();
+			result = true;
+		} catch (Exception e) {
+			tx.rollback();
+			result = false;
+		} finally {
+			em.close();
+			em = null;
+		}
+		return result;
+	}
+
+	// 여기도 내부에서 null 처리를 하시는군용! //백신으로 들어오기 때문에 null처리..
+	public static boolean updateVaccine(String vaccineName, String platform, String temper) {
+		boolean result = false;
+		EntityManager em = PublicCommon.getEntityManager();
+		EntityTransaction tx = em.getTransaction();
+
+		try {
+			tx.begin();
+			Vaccine curVaccine = em.find(Vaccine.class, vaccineName);
+			if (curVaccine != null) {
+
+				curVaccine.setPlatform(platform);
+				curVaccine.setTemperature(platform);
+
+				tx.commit();
+				result = true;
+
+			} else {
+				System.out.println("백신 정보가 존재하지 않아 수정이 불가능 합니다.");
+				tx.rollback();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			tx.rollback();
+		} finally {
+			em.close();
+			em = null;
+		}
+
+		return result;
+	}
+
+	// null처리 동일
+	public static boolean updateVaccineTargetAge(String vaccineName, int age) {
+
+		EntityManager em = PublicCommon.getEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		boolean result = false;
+
+		tx.begin();
+		try {
+			Vaccine curVaccine = em.find(Vaccine.class, vaccineName);
+
+			if (curVaccine != null) {
+				curVaccine.setTargetAge(age);
+				result = true;
+			}
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			tx.rollback();
+		} finally {
+			em.close();
+			em = null;
+		}
+
+		return result;
+
+	}
+
+	// boolean으로 바꿔서 성공 여부를 return해야할 것 같아요! //수정
+	public static boolean deleteVaccine(String vaccineName) {
+		boolean result = false;
 		EntityManager em = PublicCommon.getEntityManager();
 		EntityTransaction tx = em.getTransaction();
 
 		tx.begin();
-		if (vaccine != null) {
-			em.persist(vaccine);
+		try {
+			Vaccine curVaccine = em.find(Vaccine.class, vaccineName);
+
+			if (curVaccine != null) {
+				em.remove(curVaccine);
+			}
 
 			tx.commit();
-			em.close();
-			em = null;
-
-			return vaccine;
-		} else {
+			result = true;
+		} catch (Exception e) {
+			e.printStackTrace();
 			tx.rollback();
-
+			result = false;
+		} finally {
 			em.close();
 			em = null;
-			return null;
 		}
-	}
-	
-
-	
-	//여기도 내부에서 null 처리를 하시는군용!
-	public static void updateVaccine(Vaccine updateVaccine) {
-		if (updateVaccine != null) {
-			EntityManager em = PublicCommon.getEntityManager();
-			EntityTransaction tx = em.getTransaction();
-
-			tx.begin();
-			Vaccine curVaccine = em.find(Vaccine.class, updateVaccine.getVaccineName());
-			if (curVaccine != null) {
-
-				curVaccine.setVaccineName(updateVaccine.getVaccineName());
-				curVaccine.setTargetAge(updateVaccine.getTargetAge());
-				curVaccine.setPeriod(updateVaccine.getPeriod());
-				curVaccine.setPlatform(updateVaccine.getPlatform());
-				curVaccine.setTemperature(updateVaccine.getTemperature());
-				curVaccine.setStorage(updateVaccine.getStorage());
-
-				em.persist(curVaccine);
-				tx.commit();
-				em.close();
-				em = null;
-
-			} else {
-
-				System.out.println("백신 정보가 존재하지 않아 수정이 불가능 합니다.");
-				tx.commit();
-				em.close();
-				em = null;
-			}
-		} else {
-			EndView.errorMessage("아무것도 입력하지 않았거나 null값 입니다.");
-		}
-
-	}
-
-	//null처리 동일
-	public static boolean updateVaccineTargetAge(String vaccineName, int age) {
-		if (vaccineName != null && !vaccineName.equals("")) {
-
-			EntityManager em = PublicCommon.getEntityManager();
-			EntityTransaction tx = em.getTransaction();
-			boolean result = false;
-
-			tx.begin();
-			try {
-				Vaccine curVaccine = em.find(Vaccine.class, vaccineName);
-
-				if (curVaccine != null) {
-					curVaccine.setTargetAge(age);
-					result = true;
-				}
-
-				tx.commit();
-			} catch (Exception e) {
-				e.printStackTrace();
-				tx.rollback();
-			} finally {
-				em.close();
-				em = null;
-			}
-
-			return result;
-		} else {
-			EndView.errorMessage("아무것도 입력하지 않았거나 null값 입니다.");
-			return false;
-		}
-	}
-
-	//boolean으로 바꿔서 성공 여부를 return해야할 것 같아요!
-	public static void deleteVaccine(String vaccineName) {
-		if (vaccineName != null && !vaccineName.equals("")) {
-			EntityManager em = PublicCommon.getEntityManager();
-			EntityTransaction tx = em.getTransaction();
-
-			tx.begin();
-			try {
-				Vaccine curVaccine = em.find(Vaccine.class, vaccineName);
-
-				if (curVaccine != null) {
-					em.remove(curVaccine);
-				}
-
-				tx.commit();
-			} catch (Exception e) {
-				e.printStackTrace();
-				tx.rollback();
-			} finally {
-				em.close();
-				em = null;
-			}
-		}else {
-			EndView.errorMessage("아무것도 입력하지 않았거나 null값 입니다.");
-		}
+		return result;
 	}
 
 }
